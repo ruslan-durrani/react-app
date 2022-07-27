67e58e09e556d7a483c0d97c104a30dcc5a70c2c
M-RUSLAN-DURRANI <ruslandurrani907@gmail.com>
2022-07-27 21:49:57 +0500

    Updated App

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,31 +1,31 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 class Counter extends Component {
-    
-    render() { 
 
+    render() { 
+        const {onIncrement,onDecrement,onDelete,onEmpty,counter}=this.props;
         return (
 
-            <div>
+            <div className='row'>
                 
-                <button  className={this.getBadgeClasses()}>{this.showCount()}</button>
-                <button onClick= {()=>this.props.onIncrement(this.props.counter)} className = "btn btn-secondary m-2 btn-lg">+</button>
-                <button onClick={()=>this.props.onDecrement(this.props.counter)} className = "btn btn-secondary m-2 btn-lg">-</button>
-                <button onClick={()=>this.props.onDelete(this.props.counter)} className = "btn btn-danger m-2 btn-lg">Delete</button>
-                <button onClick={()=>this.props.onEmpty(this.props.counter)} className = "btn btn-primary m-2 btn-lg">Empty</button>
+                <div className='col'><button  className={this.getBadgeClasses()}>{this.showCount()}</button></div>
+                <div className='col'><button onClick= {()=>onIncrement(counter)} className = "btn btn-secondary m-2 btn-lg">+</button></div>
+                <div className='col'><button onClick={()=>onDecrement(counter)} className = {this.props.counter.value <=0 ? "btn btn-secondary m-2 btn-lg disabled":"btn btn-secondary m-2 btn-lg"} >-</button></div>
+                <div className='col'><button onClick={()=>onDelete(counter)} className = "btn btn-danger m-2 btn-lg  " >X</button></div>
+                <div className='col'><button onClick={()=>onEmpty(counter)} className = "btn btn-primary m-2 btn-lg">Empty</button></div>
             </div>
         );
     }
     
     getBadgeClasses() {
         let classColor = "btn btn-lg m-2 btn-";
-        classColor += (this.props.counter.value === 0) ? "warning" : "primary";
+        classColor += (this.props.counter.value <= 0) ? "warning" : "primary";
         return classColor;
     }
 
     showCount(){
         
-        return this.props.counter.value===0?'Zero':this.props.counter.value;
+        return this.props.counter.value<=0?'Zero':this.props.counter.value;
     }
 }
 export default Counter;
